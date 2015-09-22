e1fae82c283d9e60ab84a6ed858000f5ac9bb992
tan-patrick <patricktiotan@gmail.com>
2015-09-22 16:41:15 -0700

    add admin to users

--- web/config/passport.js
+++ web/config/passport.js
@@ -64,6 +64,7 @@ module.exports = function(passport) {
                         // set the user's local credentials
                         newUser.local.email    = email;
                         newUser.local.password = newUser.generateHash(password);
+                        newUser.local.isAdmin = false;
 
                         // save the user
                         newUser.save(function(err) {
--- web/models/user.js
+++ web/models/user.js
@@ -9,6 +9,7 @@ var userSchema = mongoose.Schema({
     local            : {
         email        : String,
         password     : String,
+        isAdmin      : Boolean
     },
     facebook         : {
         id           : String,
