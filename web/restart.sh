993f8dd7ea37242b07a65b52f6b3973a74246492
Vincent Kyi <vincekyi@gmail.com>
2016-06-16 00:34:45 +0000

    fixed restart script

--- web/restart.sh
+++ web/restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git pull origin dev
-node_modules/forever/bin/forever stop deploy_server.js
+node_modules/forever/bin/forever stopall
 node_modules/forever/bin/forever start deploy_server.js
