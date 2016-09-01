3c3ae3ce59e822f71f82d7b30aef1a9b7322c7ca
tan-patrick <patricktiotan@gmail.com>
2016-09-01 02:31:52 -0700

    test jenkins fix

--- web/test/test.js
+++ web/test/test.js
@@ -3,6 +3,6 @@ var app = require('../server.js');
 
 describe('GET /', function() {
     it('respond with hello world', function(done) {
-        request(app).get('/testing').expect('hello world', done);
+        request(app).get('/testing').expect('hello world!', done);
     });
 });
