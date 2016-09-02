1898f6cf991e87ccbb448fa9ca83949626fffbd2
Vincent Kyi <vincekyi@gmail.com>
2016-09-02 01:36:25 +0000

    fix restart script

--- web/scripts/restart.sh
+++ web/scripts/restart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 git pull origin dev
-echo -e "8z4r3p8bdy\n" | sudo -S su - <<'EOF'
-cd /home/developer/Aztec-web/web
-forever stopall
-forever start deploy_server.js
-EOF
+echo 8z4r3p8bdy | sudo -S forever restartall
+#echo 8z4r3p8bdy | sudo -S su - <<'EOF'
+#cd /home/developer/Aztec-web/web
+#forever restartall
+#forever start deploy_server.js
+#EOF
